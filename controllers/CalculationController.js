39d49cad99e6e5642d018676ddc2750ccec5a349
karemjouini <karemjouini97@gmail.com>
2020-11-06 15:08:15 +0100

    Added factoriel iterative, execution time in ms , heap usage

--- controllers/CalculationController.js
+++ controllers/CalculationController.js
@@ -1,9 +1,36 @@
 const calculationService = require("../services/calculationService");
-const express=require("express");
+const express = require("express");
 const router = express.Router();
-router.get("/factoriel",(req,res)=>{
+const perf = require('execution-time')();
+router.get("/factoriel", (req, res) => {
     const n = req.query.n;
-     return res.status(200).send("<h1>" + n+"! = "+calculationService.factoriel(n)+"</h1>");
- });
+    const heapUsageBefore = process.memoryUsage().heapUsed;
+    perf.start();
+    const f = calculationService.factoriel(n);
+    const result = perf.stop();
+    const heapUsageAfter = process.memoryUsage().heapUsed;
+    return res.status(200).json({
+        factoriel:
+            "<h1>" + n + "! = " + f + "</h1>",
+        memoryUsage: heapUsageAfter - heapUsageBefore,
+        executionTimeinMilliseconds: result.time
+    });
+});
 
- module.exports = router;
+router.get("/factorielIterative", (req, res) => {
+    const n = req.query.n;
+    const heapUsageBefore = process.memoryUsage().heapUsed;
+    perf.start();
+    const f = calculationService.factorielIterative(n);
+    const result = perf.stop();
+    const heapUsage = process.memoryUsage().heapUsed;
+    const heapUsageAfter = process.memoryUsage().heapUsed;
+    return res.status(200).json({
+        factoriel:
+            "<h1>" + n + "! = " + f + "</h1>",
+        memoryUsage: heapUsageAfter - heapUsageBefore,
+        executionTimeinMilliseconds: result.time
+    });
+});
+
+module.exports = router;
--- services/calculationService.js
+++ services/calculationService.js
@@ -5,5 +5,16 @@ function factoriel(n){
         return n*factoriel(n-1);
     }
 }
+function factorielIterative(n){
+    if(n==0||n==1){
+        return 1;
+    }else{
+        let f=1;
+        for(let i=2;i<=n;i++){
+            f*=i;
+        }
+        return f;
+    }
+}
 
-module.exports= {factoriel};
+module.exports= {factoriel,factorielIterative};
